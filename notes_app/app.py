47e90632f5f3b73095e47c109a00c38dc45b7618
Ronaldmolinares <108416072+Ronaldmolinares@users.noreply.github.com>
2026-08-18 14:10:01 -0500

    clase 3: uso del decorador route

--- notes_app/app.py
+++ notes_app/app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+
 
 app = Flask(__name__)
 
@@ -10,3 +11,24 @@ def create_app():
 
 # Para ejecutar la aplicacion --> flask run
 # debug en local --> flask run --debugger
+
+@app.route("/about")
+def about():
+    return "This is a simple notes app built with Flask."
+
+
+@app.route("/contact", methods=["GET", "POST"])
+def contact():
+    if request.method == "POST":
+        return "Formulario enviado correctamente.", 201
+    return "Página de contacto."
+
+@app.route("/api/info")
+def api_info():
+    data = {
+        "app_name": "Notes App",
+        "version": "1.0",
+        "description": "A simple notes application built with Flask."
+    }
+
+    return jsonify(data), 200
